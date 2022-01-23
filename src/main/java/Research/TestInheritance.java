bca1161b12311da597e0a15bcd45b1ce7a3d3f49
Jack <iatenine@gmail.com>
2022-01-23 11:38:50 -0700

    Added Logging Closes #2

---
+++ src/main/java/Research/TestInheritance.java
@@ -0,0 +1,5 @@
+package Research;
+
+public class TestInheritance extends TestPrimitiveFields{
+    private int newFiled;
+}
---
+++ src/main/java/Research/TestPrivatePrimitiveFields.java
@@ -0,0 +1,4 @@
+package Research;
+
+public class TestPrivatePrimitiveFields {
+}
